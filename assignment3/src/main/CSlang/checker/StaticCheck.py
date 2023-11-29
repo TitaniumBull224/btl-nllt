dd7f6330a215fc41a8edb6aa76d4572f178c78e0
TitaniumBull224 <titaniumyama224@gmail.com>
2023-11-30 03:43:19 +0700

    finish ass3 phase 1

--- assignment3/src/main/CSlang/checker/StaticCheck.py
+++ assignment3/src/main/CSlang/checker/StaticCheck.py
@@ -1,62 +1,187 @@
-
-"""
- * @author nhphung
-"""
-from AST import * 
+from AST import *
 from Visitor import *
 from Utils import Utils, MType
 from StaticError import *
 from functools import reduce
+from typing import Dict, List
 import copy
 
 
-class Member:
-    def __init__(self,n,t,isMu=False):
-        self.name = n
-        self.typ = t
+class Env:
+    def __str__(self):
+        pass
+
+
+class MemberEnv(Env):
+    def __init__(self, typ: Type, isMu: bool):
+        self.typ = typ
         self.isMu = isMu
-    def __str__(self):
-        return "Member("+self.name+","+str(self.typ)+","+str(self.kind)+","+str(self.isMu)+")"
 
-class BKClass:
-    def __init__(self,n,p,m):
-        self.name = n
-        self.parent = p
-        self.member = m
-    def __str__(self):
-        return "Class("+self.name+","+(self.parent if type(self.parent) == str else "None" if self.parent is None else self.parent.name)+",["+",".join(str(i) for i in self.member) +"])"
+    # def __str__(self):
+    #     return (
+    #         "MemberEnv(" + str(self.typ) + ", " + "Mutable"
+    #         if self.isMu
+    #         else "Immutable" + ")"
+    #     )
 
 
-class StaticChecker(BaseVisitor,Utils):
-    inttype = IntType()
-    floattype = FloatType()
-    voidtype = VoidType()
-    booltype = BoolType()
-    stringtype = StringType() 
-    def __init__(self,ast):
+class AttributeEnv(MemberEnv):
+    def __init__(self, typ: Type, isMu: bool):
+        super().__init__(typ, isMu)
+
+    # def __str__(self):
+    #     return (
+    #         "AttributeEnv(" + str(self.typ) + ", " + "Mutable"
+    #         if self.isMu
+    #         else "Immutable" + ")"
+    #     )
+
+
+class MethodEnv(MemberEnv):
+    def __init__(self, typ: Type, body: Dict):
+        self.typ = typ
+        self.isMu = False
+        self.body = body
+
+    # def __str__(self):
+    #     return (
+    #         "MethodEnv(" + str(self.typ) + ", " + "Mutable"
+    #         if self.isMu
+    #         else "Immutable" + str(self.body) + ")"
+    #     )
+
+
+class ClassEnv(Env):
+    def __init__(self, parent: str, member: Dict):
+        self.parent = parent
+        self.member = member
+
+    # def __str__(self):
+    #     return "ClassEnv(" + self.parent + ", " + str(self.member) + ")"
+
+
+class GetEnv(BaseVisitor, Utils):
+    def visitProgram(self, ast: Program, o: object):
+        o = reduce(lambda prev, curr: self.visit(curr, prev), ast.decl, o)
+        return o
+
+    def visitClassDecl(self, ast: ClassDecl, o: object):
+        id = ast.classname.name
+        parentId = ast.parentname.name if ast.parentname else None
+
+        if o.get(id) is not None:
+            raise Redeclared(Class(), id)
+
+        env = reduce(lambda prev, curr: self.visit(curr, prev), ast.memlist, {})
+        o[id] = ClassEnv(parentId, env)
+        return o
+
+    def visitAttributeDecl(self, ast: AttributeDecl, o: object):
+        o = self.visit(ast.decl, o)
+        return o
+
+    def visitVarDecl(self, ast: VarDecl, o: object):
+        id = ast.variable.name
+
+        if o.get(id) is not None:
+            raise Redeclared(Attribute(), id)
+
+        o[id] = AttributeEnv(ast.varType, True)
+        return o
+
+    def visitConstDecl(self, ast: ConstDecl, o: object):
+        id = ast.constant.name
+
+        if o.get(id) is not None:
+            raise Redeclared(Attribute(), id)
+
+        o[id] = AttributeEnv(ast.constType, False)
+        return o
+
+    def visitMethodDecl(self, ast: MethodDecl, o: object):
+        id = ast.name.name
+
+        if o.get(id) is not None:
+            raise Redeclared(Method(), id)
+        body = {}
+
+        for param in ast.param:
+            if body.get(param.variable.name) is not None:
+                raise Redeclared(Parameter(), param.variable.name)
+            body[param.variable.name] = AttributeEnv(param.varType, True)
+            if "constructor" in id:
+                id += str(param.varType).replace("Type", "")
+
+        if id == "constructor":
+            id += "Default"
+
+        for stmt in ast.body.stmt:
+            if type(stmt) is VarDecl:
+                if body.get(stmt.variable.name) is not None:
+                    raise Redeclared(Variable(), stmt.variable.name)
+                body[stmt.variable.name] = AttributeEnv(stmt.varType, True)
+
+            if type(stmt) is ConstDecl:
+                if body.get(stmt.constant.name) is not None:
+                    raise Redeclared(Constant(), stmt.constant.name)
+                body[stmt.constant.name] = AttributeEnv(stmt.constType, False)
+
+        o[id] = MethodEnv(ast.returnType, body)
+        return o
+
+
+class EnvScope:
+    def __init__(self, ast: Program, o: object):
+        self.env = GetEnv().visit(ast, o)
+
+    def searchGlobal(self, classId: str):
+        if self.env.get(classId) is None:
+            return False
+        return True
+
+    def searchCLassScope(self, classId: str, memberId: str):
+        it = classId
+        while self.env.get(it) is not None:
+            classEnv = self.env.get(it)
+            if memberId in classEnv.member.keys():
+                return True
+            if classEnv.parent == None:
+                return False
+            it = classEnv.parent
+        return False
+
+    def searchMethodScope(self):
+        return True
+
+    def searchStmtScope(self):
+        return True
+
+
+class StaticChecker(BaseVisitor, Utils):
+    def __init__(self, ast):
         self.ast = ast
-        self.io = [BKClass("io",None,[
-                            Member("@readInt",MType([],StaticChecker.inttype),False),
-                            Member("@writeIntLn",MType([StaticChecker.inttype],StaticChecker.voidtype),False),
-                            ])]
+        self.io = {
+            "io": ClassEnv(
+                None,
+                {
+                    "@readInt": MethodEnv(IntType(), {}),
+                    "@writeIntLn": MethodEnv(VoidType(), {"anArg": IntType()}),
+                    "@readFloat": MethodEnv(FloatType(), {}),
+                    "@writeFloatLn": MethodEnv(VoidType(), {"anArg": FloatType()}),
+                    "@readBool": MethodEnv(BoolType(), {}),
+                    "@writeBoolLn": MethodEnv(VoidType(), {"anArg": BoolType()}),
+                    "@readStr": MethodEnv(StringType(), {}),
+                    "@writeStrLn": MethodEnv(VoidType(), {"anArg": StringType()}),
+                },
+            )
+        }
+
     def check(self):
-        self.visit(self.ast,self.io)
-        return "successful"
+        self.visit(self.ast, self.io)
+        return []
 
-    def visitProgram(self,ast, c):
-        env = reduce(lambda a,e: self.visit(e,a), ast.decl,c)
-        
-    def visitClassDecl(self, ast, c):
-        if ast.classname.name in map(lambda x: x.name,c):
-            raise Redeclared(Class(),ast.classname.name)
-        mem = reduce(lambda a,e: self.visit(e,a) ,ast.memlist,[])
-        return [BKClass(ast.classname.name,ast.parentname,mem)]+c
-
-    def visitAttributeDecl(self,ast,c):
-        field = self.visit(ast.decl,c) 
-        return [field]+c
-
-    def visitVarDecl(self, ast, c):
-        if ast.variable.name in map(lambda x: x.name,c):
-            raise Redeclared(Attribute(),ast.variable.name)
-        return Member(ast.variable.name,ast.varType,True)
+    def visitProgram(self, ast: Program, o: object):
+        o = EnvScope(ast, o)
+        if o.searchCLassScope("Program", "@main") == False:
+            raise NoEntryPoint()
+        return o
--- assignment3/src/test/CheckSuite.py
+++ assignment3/src/test/CheckSuite.py
@@ -1,23 +1,222 @@
 import unittest
 from TestUtils import TestChecker
 from AST import *
- 
+
+
 class CheckSuite(unittest.TestCase):
-    def test_redeclared_class(self):
-        input = """class a {} class b {} class a {}"""
+    def test0(self):
+        input = """
+            class Program {
+                func @main(): void {}
+            }
+            class a {}
+            class b {}
+            class a {}
+        """
         expect = "Redeclared Class: a"
-        self.assertTrue(TestChecker.test(input,expect,400))
-    def test_redeclared_attribute(self):
-        input = """class a {var a:int;var c:int;var c:int;}"""
-        expect = "Redeclared Attribute: c"
-        self.assertTrue(TestChecker.test(input,expect,401))
-    
-    def test_redeclared_class_with_ast(self):
-        input = Program([ClassDecl(Id("a"),[]),ClassDecl(Id("b"),[]),ClassDecl(Id("a"),[])])
+        self.assertTrue(TestChecker.test(input, expect, 400))
+
+    def test1(self):
+        input = """
+            class Program {}
+        """
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input, expect, 401))
+
+    def test2(self):
+        input = """
+            class Program {
+                func @main(): void {}
+            }
+            class a {}
+            class b {}
+            class b <- a {}
+        """
         expect = "Redeclared Class: a"
-        self.assertTrue(TestChecker.test(input,expect,402))
-    def test_redeclared_attribute_with_ast(self):
-        input = Program([ClassDecl(Id("a"),[AttributeDecl(VarDecl(Id("a"),IntType())),AttributeDecl(VarDecl(Id("c"),IntType())),AttributeDecl(VarDecl(Id("c"),IntType()))])])
+        self.assertTrue(TestChecker.test(input, expect, 402))
+
+    def test3(self):
+        input = """
+            class Program {
+                func @main(): void {}
+            }
+            class Program {
+                func @main(): void {}
+            }
+        """
+        expect = "Redeclared Class: Program"
+        self.assertTrue(TestChecker.test(input, expect, 403))
+
+    def test4(self):
+        input = """
+            class a {}
+        """
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input, expect, 404))
+
+    def test5(self):
+        input = """
+            class Program {
+                var a: int = 1;
+                const a: float = 1.0;
+                func @main(): void {}
+            }
+        """
+        expect = "Redeclared Attribute: a"
+        self.assertTrue(TestChecker.test(input, expect, 405))
+
+    def test6(self):
+        input = """
+            class Program {
+                var a, b: int = 1, 2;
+                const b: float = 1.0;
+                func @main(): void {}
+            }
+        """
+        expect = "Redeclared Attribute: b"
+        self.assertTrue(TestChecker.test(input, expect, 406))
+
+    def test7(self):
+        input = """
+            class Program {
+                var a: int = 1;
+                func a(): int {}
+                func @main(): void {}
+            }
+        """
+        expect = "Redeclared Method: a"
+        self.assertTrue(TestChecker.test(input, expect, 407))
+
+    def test8(self):
+        input = """
+            class Program {
+                func @main(): void {}
+            }
+            class Program <- a {
+                func @main(): void {}
+                var a, @main: int = 1, 2; 
+            }
+        """
+        expect = "Redeclared Attribute: @main"
+        self.assertTrue(TestChecker.test(input, expect, 408))
+
+    def test9(self):
+        input = """
+            class Program {
+                func @main(): void {}
+                func hololive(member: string): int {}
+                var member: int;
+                const hololive: float;
+            }
+        """
+        expect = "Redeclared Attribute: hololive"
+        self.assertTrue(TestChecker.test(input, expect, 409))
+
+    def test10(self):
+        input = """
+            class a {
+                var b: int = 1;
+            }
+            class a <- Program {
+                func @main(): void {}
+                var b, c: float = 1.1, 1.2;
+                const c: bool = true;
+            }
+        """
         expect = "Redeclared Attribute: c"
-        self.assertTrue(TestChecker.test(input,expect,404))    
-    
+        self.assertTrue(TestChecker.test(input, expect, 410))
+
+    def test11(self):
+        input = """
+            class Program {
+                func @main(): void {}
+                func a(b: int): int {}
+                func a(c: float): float {}
+            }
+        """
+        expect = "Redeclared Method: a"
+        self.assertTrue(TestChecker.test(input, expect, 411))
+
+    def test12(self):
+        input = """
+            class Program {
+                func @main(): void {}
+                var a: int = 1;
+                func a(c: float): float {}
+            }
+        """
+        expect = "Redeclared Method: a"
+        self.assertTrue(TestChecker.test(input, expect, 412))
+
+    def test13(self):
+        input = """
+            class Program {
+                func @main(): void {}
+                func constructor(a: float) {}
+                func constructor(b: float) {}
+                func @main(): void {}
+            }
+        """
+        expect = "Redeclared Method: @main"
+        self.assertTrue(TestChecker.test(input, expect, 413))
+
+    def test14(self):
+        input = """
+            class Program {
+                func @main(): void {}
+                func main(): void {}
+            }
+            class io {}
+        """
+        expect = "Redeclared Class: io"
+        self.assertTrue(TestChecker.test(input, expect, 414))
+
+    def test15(self):
+        input = """
+            class Program {
+                func @main(): void {}
+            }
+            class a {}
+            class a <- a {}
+        """
+        expect = "Redeclared Class: a"
+        self.assertTrue(TestChecker.test(input, expect, 415))
+
+    def test16(self):
+        input = """
+            class b {
+                func @main(): void {}
+            }
+            class a {
+                func @main(): void {}
+            }
+        """
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input, expect, 416))
+
+    def test17(self):
+        input = """
+            class program {
+                func @main(): void {}
+            }
+        """
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input, expect, 417))
+
+    def test18(self):
+        input = """
+            class Program {
+                func @Main(): void {}
+            }
+        """
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input, expect, 418))
+
+    def test19(self):
+        input = """
+            class Program {
+                func main(): void {}
+            }
+        """
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input, expect, 419))
